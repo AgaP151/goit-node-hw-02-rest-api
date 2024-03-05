73d8b24e986ef02bd61fb95535f5811f940ee6e7
AgaP151 <piotrowska.agnieszka1987@gmail.com>
2024-03-05 23:24:12 +0100

    others

--- services/authenticate.js
+++ services/authenticate.js
@@ -1,13 +1,13 @@
 const jwt = require("jsonwebtoken");
 const { User } = require("../schemas/authSchema");
-const { HttpError } = require("../helpers/HttpError");
+const HttpError = require("../helpers/HttpError");
 const { SECRET_KEY } = process.env;
 
 const authenticate = async (req, res, next) => {
   const { authorization = "" } = req.headers;
   const [bearer, token] = authorization.split(" ");
   if (bearer !== "Bearer") {
-    next(HttpError(401));
+    return next(HttpError(401));
   }
   try {
     const { id } = jwt.verify(token, SECRET_KEY);
@@ -18,7 +18,7 @@ const authenticate = async (req, res, next) => {
     req.user = user;
     next();
   } catch {
-    next(HttpError(401, "Not authorized"));
+    return next(HttpError(401, "Not authorized"));
   }
 };
 module.exports = authenticate;
