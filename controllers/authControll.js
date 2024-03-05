130d271a2aec4527f7d8303b185f9e0422bf3386
AgaP151 <piotrowska.agnieszka1987@gmail.com>
2024-03-05 23:15:49 +0100

    implemented avatar for new user registration and added avatar update

--- controllers/authControll.js
+++ controllers/authControll.js
@@ -3,6 +3,12 @@ const jwt = require("jsonwebtoken");
 const { User } = require("../schemas/authSchema");
 const { HttpError } = require("../helpers/HttpError");
 const { SECRET_KEY } = process.env;
+const gravatar = require("gravatar");
+const path = require("path");
+const fs = require("fs/promises");
+const jimp = require("jimp");
+
+const avatarsDir = path.join(__dirname, "../", "public", "avatars");
 
 const register = async (req, res) => {
   const { email, password } = req.body;
@@ -12,7 +18,13 @@ const register = async (req, res) => {
     throw HttpError(409, "Email already in use");
   }
   const hashPassword = await bcrypt.hash(password, 10);
-  const newUser = await User.create({ ...req.body, password: hashPassword });
+  const avatarURL = gravatar.url(email);
+
+  const newUser = await User.create({
+    ...req.body,
+    password: hashPassword,
+    avatarURL,
+  });
 
   res.status(201).json({
     name: newUser.name,
@@ -68,9 +80,41 @@ const logout = async (req, res) => {
   });
 };
 
+const updateAvatar = async (req, res, next) => {
+  const { path: tempUpload } = req.file;
+  // const { path: tempUpload } = req.file;
+  try {
+    const avatar = await jimp.read(tempUpload);
+    await avatar.resize(250, 250).write(tempUpload);
+  } catch (error) {
+    return next(error);
+  }
+
+  const { _id } = req.user;
+  const filename = `${_id}_${Date.now()}.jpg`;
+  const resultUpload = path.join(avatarsDir, filename);
+
+  try {
+    await fs.rename(tempUpload, resultUpload);
+  } catch (error) {
+    return next(error);
+  }
+
+  const avatarURL = path.join("avatars", filename);
+  try {
+    await User.findByIdAndUpdate(_id, { avatarURL });
+    return res.status(200).json({
+      avatarURL,
+    });
+  } catch (error) {
+    return next(error);
+  }
+};
+
 module.exports = {
   register,
   login,
   getCurrent,
   logout,
+  updateAvatar,
 };
